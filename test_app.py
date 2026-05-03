3b676796ba503704218aff817cc08996de19e633
BhanupatelGit <bhanupatel1704@gmail.com>
2026-05-03 12:55:35 +0530

    fix lint issues - follow PEP8

--- test_app.py
+++ test_app.py
@@ -1,14 +1,17 @@
 from app import app
 
+
 def test_home_status():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_home_content():
     client = app.test_client()
     response = client.get("/")
     assert b"Hello CI/CD" in response.data
+
 
 def test_invalid_route():
     client = app.test_client()
