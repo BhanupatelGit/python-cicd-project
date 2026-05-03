236c47329f613257fcf13007997ca20ac114e297
BhanupatelGit <bhanupatel1704@gmail.com>
2026-05-03 12:48:05 +0530

    enhanced test coverage

--- test_app.py
+++ test_app.py
@@ -1,6 +1,16 @@
 from app import app
 
-def test_home():
+def test_home_status():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+
+def test_home_content():
+    client = app.test_client()
+    response = client.get("/")
+    assert b"Hello CI/CD" in response.data
+
+def test_invalid_route():
+    client = app.test_client()
+    response = client.get("/invalid")
+    assert response.status_code == 404
